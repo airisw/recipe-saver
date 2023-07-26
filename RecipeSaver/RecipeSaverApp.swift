d085c4518d5463ba69c962ed2d07ce8f24456ba0
Airis <airiswang@hotmail.com>
2023-07-26 11:30:45 -0400

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Airis Wang on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
